34a3438814669ea93529189b39eb62194e974806
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-04-18 17:23:40 -0600

    changed file structure and updated config file

--- YORPLib/YORPLib/CMake/YORPLibConfig.cmake
+++ YORPLib/YORPLib/CMake/YORPLibConfig.cmake
@@ -22,30 +22,7 @@
 #
 
 
-if (EXISTS /home/bebe0705/.am_fortuna)
-	set(IS_FORTUNA ON)
-	message("-- This is Fortuna")
-
-else()
-	set(IS_FORTUNA OFF)
-endif()
-
-
-if(${IS_FORTUNA})
-	set(YORPLIB_INCLUDE_HEADER /home/bebe0705/libs/local/include/YORPLib/)
-	set(YORPLIB_LIBRARY /home/bebe0705/libs/local/lib/libYORPLib.so)
-
-else()
-	set(YORPLIB_INCLUDE_HEADER /usr/local/include/YORPLib/)
-
-	if (APPLE)
-		set(YORPLIB_LIBRARY /usr/local/lib/libYORPLib.dylib)
-	elseif(UNIX AND NOT APPLE)
-		set(YORPLIB_LIBRARY /usr/local/lib/libYORPLib.so)
-	else()
-		message(FATAL_ERROR "Unsupported platform")
-	endif()
-endif()
-
+set(YORPLIB_INCLUDE_HEADER /home/bebe0705/libs/local/include/YORPLib/)
+set(YORPLIB_LIBRARY /home/bebe0705/libs/local/lib/libYORPLib.so)
 
 message("-- Found YORPLib: " ${YORPLIB_LIBRARY})
